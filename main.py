b36cf623b48fe820803fafb282532c3a29751cb1
Pratik Biswas <82697807+Pratt2762@users.noreply.github.com>
2023-06-14 12:13:51 +0530

    Added new synthetic data to our original dataset

--- main.py
+++ main.py
@@ -17,6 +17,7 @@ from nltk.tokenize import word_tokenize
 
 import data_preprocessing
 from data_preprocessing import format_text
+from synthetic_data_addition import add_synthetic_data
 
 from bert_classifier import BertClassifier, train, evaluate_bert, evaluate_classwise_bert
 
@@ -37,9 +38,15 @@ dataset['text'] = dataset['text'].map(str)
 # Check to see if indeed the 'text' column is of dtype string
 dataset.dtypes
 
+
+# Adding some extra synthetic data (manually generated) to our data to increase its diversity
+dataset = add_synthetic_data(dataset)
+
+
 # Applying the necessary preprocessing techniques to our data
 for i in range(dataset.shape[0]):
   dataset.iloc[i,1] = format_text(dataset.iloc[i,1])
+  
   
 # Displaying our dataset after applying the preprocessing techniques to it
 dataset
