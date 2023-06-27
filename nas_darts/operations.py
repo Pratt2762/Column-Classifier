c7579ae9876cd1a248d85eeb83ed5fc9597a6021
Pratik Biswas <82697807+Pratt2762@users.noreply.github.com>
2023-06-27 08:03:31 +0530

    Update operations.py

--- nas_darts/operations.py
+++ nas_darts/operations.py
@@ -15,83 +15,80 @@ OPS = {
 
 
 class ReLUConvBN(nn.Module):
-  def __init__(self, C_in, C_out, kernel_size, stride, padding, affine=True):
-    super(ReLUConvBN, self).__init__()
-    self.op = nn.Sequential(
-      nn.ReLU(inplace=False),
-      nn.Conv1d(C_in, C_out, kernel_size, stride=stride, padding=padding, bias=False),
-      nn.BatchNorm1d(C_out, affine=affine)
-    )
+    def __init__(self, C_in, C_out, kernel_size, stride, padding, affine=True):
+        super(ReLUConvBN, self).__init__()
+        self.op = nn.Sequential(
+          nn.ReLU(inplace=False),
+          nn.Conv1d(C_in, C_out, kernel_size, stride=stride, padding=padding, bias=False),
+          nn.BatchNorm1d(C_out, affine=affine)
+        )
     
-  def forward(self, x):
-    return self.op(x)
+    def forward(self, x):
+        return self.op(x)
 
 
 class DilConv(nn.Module):    
-  def __init__(self, C_in, C_out, kernel_size, stride, padding, dilation, affine=True):
-    super(DilConv, self).__init__()
-    self.op = nn.Sequential(
-      nn.ReLU(inplace=False),
-      nn.Conv1d(C_in, C_in, kernel_size=kernel_size, stride=stride, padding=padding, dilation=dilation, groups=C_in, bias=False),
-      nn.Conv1d(C_in, C_out, kernel_size=1, padding=0, bias=False),
-      nn.BatchNorm1d(C_out, affine=affine),
-      )
+    def __init__(self, C_in, C_out, kernel_size, stride, padding, dilation, affine=True):
+        super(DilConv, self).__init__()
+        self.op = nn.Sequential(
+          nn.ReLU(inplace=False),
+          nn.Conv1d(C_in, C_in, kernel_size=kernel_size, stride=stride, padding=padding, dilation=dilation, groups=C_in, bias=False),
+          nn.Conv1d(C_in, C_out, kernel_size=1, padding=0, bias=False),
+          nn.BatchNorm1d(C_out, affine=affine),
+        )
 
-  def forward(self, x):
-    return self.op(x)
+    def forward(self, x):
+        return self.op(x)
 
 
 class SepConv(nn.Module):
-  def __init__(self, C_in, C_out, kernel_size, stride, padding, affine=True):
-    super(SepConv, self).__init__()
-    self.op = nn.Sequential(
-      nn.ReLU(inplace=False),
-      nn.Conv1d(C_in, C_in, kernel_size=kernel_size, stride=stride, padding=padding, groups=C_in, bias=False),
-      nn.Conv1d(C_in, C_in, kernel_size=1, padding=0, bias=False),
-      nn.BatchNorm1d(C_in, affine=affine),
-      nn.ReLU(inplace=False),
-      nn.Conv1d(C_in, C_in, kernel_size=kernel_size, stride=1, padding=padding, groups=C_in, bias=False),
-      nn.Conv1d(C_in, C_out, kernel_size=1, padding=0, bias=False),
-      nn.BatchNorm1d(C_out, affine=affine),
-      )
+    def __init__(self, C_in, C_out, kernel_size, stride, padding, affine=True):
+        super(SepConv, self).__init__()
+        self.op = nn.Sequential(
+          nn.ReLU(inplace=False),
+          nn.Conv1d(C_in, C_in, kernel_size=kernel_size, stride=stride, padding=padding, groups=C_in, bias=False),
+          nn.Conv1d(C_in, C_in, kernel_size=1, padding=0, bias=False),
+          nn.BatchNorm1d(C_in, affine=affine),
+          nn.ReLU(inplace=False),
+          nn.Conv1d(C_in, C_in, kernel_size=kernel_size, stride=1, padding=padding, groups=C_in, bias=False),
+          nn.Conv1d(C_in, C_out, kernel_size=1, padding=0, bias=False),
+          nn.BatchNorm1d(C_out, affine=affine),
+        )
 
-  def forward(self, x):
-    return self.op(x)
+    def forward(self, x):
+        return self.op(x)
 
 
 class Identity(nn.Module):
-  def __init__(self):
-    super(Identity, self).__init__()
+    def __init__(self):
+        super(Identity, self).__init__()
 
-  def forward(self, x):
-    return x
+    def forward(self, x):
+        return x
 
 
 class Zero(nn.Module):
-  def __init__(self, stride):
-    super(Zero, self).__init__()
-    self.stride = stride
+    def __init__(self, stride):
+        super(Zero, self).__init__()
+        self.stride = stride
 
-  def forward(self, x):
-    if self.stride == 1:
-      return x.mul(0.)
-    return x[:,:,::self.stride].mul(0.)
+    def forward(self, x):
+        if self.stride == 1:
+            return x.mul(0.)
+        return x[:,:,::self.stride].mul(0.)
 
 
 class FactorizedReduce(nn.Module):
-  def __init__(self, C_in, C_out, affine=True):
-    super(FactorizedReduce, self).__init__()
-    assert C_out % 2 == 0
-    self.relu = nn.ReLU(inplace=False)
-    self.conv_1 = nn.Conv1d(C_in, C_out // 2, 1, stride=2, padding=0, bias=False)
-    self.conv_2 = nn.Conv1d(C_in, C_out // 2, 1, stride=2, padding=0, bias=False) 
-    self.bn = nn.BatchNorm1d(C_out, affine=affine)
+    def __init__(self, C_in, C_out, affine=True):
+        super(FactorizedReduce, self).__init__()
+        assert C_out % 2 == 0
+        self.relu = nn.ReLU(inplace=False)
+        self.conv_1 = nn.Conv1d(C_in, C_out // 2, 1, stride=2, padding=0, bias=False)
+        self.conv_2 = nn.Conv1d(C_in, C_out // 2, 1, stride=2, padding=0, bias=False) 
+        self.bn = nn.BatchNorm1d(C_out, affine=affine)
 
-  def forward(self, x):
-    x = self.relu(x)
-    out = torch.cat([self.conv_1(x), self.conv_2(x[:,:,1:,1:])], dim=1)
-    out = self.bn(out)
-    return out
-            weights: weight for each operation
-        """
-        return sum(w * op(x) for w, op in zip(weights, self._ops))
+    def forward(self, x):
+        x = self.relu(x)
+        out = torch.cat([self.conv_1(x), self.conv_2(x[:,:,1:,1:])], dim=1)
+        out = self.bn(out)
+        return out
