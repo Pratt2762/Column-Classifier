d4c624b4191915ddf1f900bf0523a90edd4763b7
Pratik Biswas <82697807+Pratt2762@users.noreply.github.com>
2023-06-29 13:10:07 +0530

    Update train.py

--- nas_darts/train.py
+++ nas_darts/train.py
@@ -17,28 +17,51 @@ from model import Network as Network
 from character_embeddings import create_dataset
 
 
-parser = argparse.ArgumentParser("text_classify")
-parser.add_argument('--data', type=str, default='data.csv', help='location of the data corpus')
-parser.add_argument('--batch_size', type=int, default=4, help='batch size')
-parser.add_argument('--learning_rate', type=float, default=0.025, help='init learning rate')
-parser.add_argument('--momentum', type=float, default=0.9, help='momentum')
-parser.add_argument('--weight_decay', type=float, default=3e-4, help='weight decay')
-parser.add_argument('--report_freq', type=float, default=50, help='report frequency')
-parser.add_argument('--gpu', type=int, default=0, help='gpu device id')
-parser.add_argument('--epochs', type=int, default=10, help='num of training epochs')
-parser.add_argument('--init_channels', type=int, default=68, help='num of init channels')
-parser.add_argument('--layers', type=int, default=10, help='total number of layers')
-parser.add_argument('--model_path', type=str, default='saved_models', help='path to save the model')
-parser.add_argument('--auxiliary', action='store_true', default=False, help='use auxiliary tower')
-parser.add_argument('--auxiliary_weight', type=float, default=0.4, help='weight for auxiliary loss')
-parser.add_argument('--cutout', action='store_true', default=False, help='use cutout')
-parser.add_argument('--cutout_length', type=int, default=16, help='cutout length')
-parser.add_argument('--drop_path_prob', type=float, default=0.2, help='drop path probability')
-parser.add_argument('--save', type=str, default='EXP', help='experiment name')
-parser.add_argument('--seed', type=int, default=0, help='random seed')
-parser.add_argument('--arch', type=str, default='DARTS', help='which architecture to use')
-parser.add_argument('--grad_clip', type=float, default=5, help='gradient clipping')
-args = parser.parse_args()
+# parser = argparse.ArgumentParser("text_classify")
+# parser.add_argument('--data', type=str, default='data.csv', help='location of the data corpus')
+# parser.add_argument('--batch_size', type=int, default=4, help='batch size')
+# parser.add_argument('--learning_rate', type=float, default=0.025, help='init learning rate')
+# parser.add_argument('--momentum', type=float, default=0.9, help='momentum')
+# parser.add_argument('--weight_decay', type=float, default=3e-4, help='weight decay')
+# parser.add_argument('--report_freq', type=float, default=50, help='report frequency')
+# parser.add_argument('--gpu', type=int, default=0, help='gpu device id')
+# parser.add_argument('--epochs', type=int, default=10, help='num of training epochs')
+# parser.add_argument('--init_channels', type=int, default=68, help='num of init channels')
+# parser.add_argument('--layers', type=int, default=10, help='total number of layers')
+# parser.add_argument('--model_path', type=str, default='saved_models', help='path to save the model')
+# parser.add_argument('--auxiliary', action='store_true', default=False, help='use auxiliary tower')
+# parser.add_argument('--auxiliary_weight', type=float, default=0.4, help='weight for auxiliary loss')
+# parser.add_argument('--cutout', action='store_true', default=False, help='use cutout')
+# parser.add_argument('--cutout_length', type=int, default=16, help='cutout length')
+# parser.add_argument('--drop_path_prob', type=float, default=0.2, help='drop path probability')
+# parser.add_argument('--save', type=str, default='EXP', help='experiment name')
+# parser.add_argument('--seed', type=int, default=0, help='random seed')
+# parser.add_argument('--arch', type=str, default='DARTS', help='which architecture to use')
+# parser.add_argument('--grad_clip', type=float, default=5, help='gradient clipping')
+# args = parser.parse_args()
+
+class Args:
+    data = "data.csv"
+    batch_size = 4
+    learning_rate = 0.025
+    momentum = 0.9
+    weight_decay = 3e-4
+    report_freq = 50
+    gpu = 0
+    epochs = 10
+    init_channels = 68
+    layers = 10
+    model_path = "saved_models"
+    auxiliary = False
+    auxiliary_weight = 0.4
+    drop_path_prob = 0.2
+    save = "EXP"
+    seed = 0
+    arch = "DARTS"
+    grad_clip = 5.0
+    
+args = Args()
+
 
 args.save = 'eval-{}-{}'.format(args.save, time.strftime("%Y%m%d-%H%M%S"))
 utils.create_exp_dir(args.save, scripts_to_save=glob.glob('*.py'))
